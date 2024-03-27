b6f25c062873d4e24f5c41b06d116444b65003be
gohna <2302094@sit.singaporetech.edu.sg>
2024-03-27 10:27:31 +0800

    1st commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="css/main.css">
+<script defer src="js/main.js"></script>
+
+<!-- Load an icon library to show a hamburger menu (bars) on small screens -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="topnav" id="myTopnav">
+  <a href="/index.php" class="active">Home</a>
+  <a href="/content.php">Content</a>
+  <a href="#contact">Contact</a>
+  <a href="/about.php">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
