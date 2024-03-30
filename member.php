b7ad95d5e60c41ddd4a97242789704ebbf87bdc2
lzj1899 <145450814+lzj1899@users.noreply.github.com>
2024-03-30 13:25:17 +0800

    Updated inc

--- member.php
+++ member.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login/Register Page</title>
+    <title>Member Page</title>
     <?php
         include "INC/head.inc.php";
     ?>
@@ -17,5 +17,8 @@
         <div id="app" class="login"></div>
         <script src="JSX/login_register.js"></script>
     </main>
+    <?php
+        include "INC/footer.inc.php";
+    ?>
 </body>
 </html>
