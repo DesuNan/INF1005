1ee40b7108513e3544aafcedc77a48835a3e8839
TzeYaw <hewtzeyaw@gmail.com>
2024-04-02 00:39:49 +0800

    Minor changes

--- content.php
+++ content.php
@@ -2,17 +2,16 @@
 <html lang="en">
 
 <head>
-    <?php
-        include "inc/nav.inc.php";
-        include "inc/nav.inc.php";
-    ?>
     <title>Coursedemy</title>
     <?php
     include "inc/head.inc.php";
     ?>
 </head>
-<body>
 
+<body>
+    <?php
+    include "inc/nav2.inc.php";
+    ?>
     <main class="container">
         <section id="languages" class="section-border">
             <h2 style="padding-top: 120px;">Languages</h2>
@@ -21,13 +20,13 @@
                     <h3 style="color:#555;">English</h3>
                     <figure>
                         <a target="_self" href="english.php" class="logothumbnail">
-                            <img class="img-popup" src="img/english.jpg" alt="English" title="View larger image..." >
+                            <img class="img-popup" src="img/english.jpg" alt="English" title="View larger image...">
                         </a>
                     </figure>
                     <p style="color: #666; line-height: 1.6;">
-                    Unlock the world with English! As the global language of communication, learning English opens doors to endless opportunities. 
-                    Whether you're traveling, studying, or pursuing a career, English proficiency empowers you to connect with people from diverse 
-                    cultures and backgrounds. From enhancing job prospects to enriching cultural experiences, mastering English enriches your life in countless ways.
+                        Unlock the world with English! As the global language of communication, learning English opens doors to endless opportunities.
+                        Whether you're traveling, studying, or pursuing a career, English proficiency empowers you to connect with people from diverse
+                        cultures and backgrounds. From enhancing job prospects to enriching cultural experiences, mastering English enriches your life in countless ways.
                     </p>
                 </article>
             </div>
@@ -38,14 +37,14 @@
                 <article class="col-sm">
                     <figure>
                         <a href="math.php" class="logothumbnail">
-                            <img class="img-popup" src="img/math.jpg" alt="Math" title="View larger image..." >
+                            <img class="img-popup" src="img/math.jpg" alt="Math" title="View larger image...">
                         </a>
                     </figure>
                     <p style="color: #666; line-height: 1.6;">
-                    Discover the magic of mathematics! Dive into a world where numbers, patterns, and equations come alive to solve problems and unlock 
-                    mysteries. Mathematics is the language of logic and reason, guiding us through the complexities of the universe. From calculating 
-                    everyday expenses to unraveling the secrets of the cosmos, math empowers us to understand the world around us and make informed decisions. 
-                    Embrace the beauty of numbers and embark on a journey of discovery that will sharpen your mind and enrich your life.
+                        Discover the magic of mathematics! Dive into a world where numbers, patterns, and equations come alive to solve problems and unlock
+                        mysteries. Mathematics is the language of logic and reason, guiding us through the complexities of the universe. From calculating
+                        everyday expenses to unraveling the secrets of the cosmos, math empowers us to understand the world around us and make informed decisions.
+                        Embrace the beauty of numbers and embark on a journey of discovery that will sharpen your mind and enrich your life.
                     </p>
                 </article>
             </div>
@@ -57,36 +56,36 @@
                     <h3 style="color:#555;">Chemistry</h3>
                     <figure>
                         <a href="chemistry.php" class="logothumbnail">
-                            <img class="img-popup" src="img/chemistry.jpg" alt="Chemistry" title="View larger image..." >
+                            <img class="img-popup" src="img/chemistry.jpg" alt="Chemistry" title="View larger image...">
                         </a>
                     </figure>
-                    <p style="color: #666; line-height: 1.6;">  
-                    Embark on a journey into the fascinating world of chemistry! From the tiniest atoms to the complex molecules that make up our world, chemistry 
-                    is the science that reveals the secrets of matter and its transformations. By studying chemistry, you unlock a deeper understanding of everything 
-                    around you, from the air we breathe to the food we eat and the medicines that heal us. With chemistry, you gain the power to innovate, create, and 
-                    contribute to solving some of the world's most pressing challenges. Embrace the excitement of discovery and let chemistry ignite your passion for 
-                    understanding the building blocks of life itself!
+                    <p style="color: #666; line-height: 1.6;">
+                        Embark on a journey into the fascinating world of chemistry! From the tiniest atoms to the complex molecules that make up our world, chemistry
+                        is the science that reveals the secrets of matter and its transformations. By studying chemistry, you unlock a deeper understanding of everything
+                        around you, from the air we breathe to the food we eat and the medicines that heal us. With chemistry, you gain the power to innovate, create, and
+                        contribute to solving some of the world's most pressing challenges. Embrace the excitement of discovery and let chemistry ignite your passion for
+                        understanding the building blocks of life itself!
                     </p>
                 </article>
                 <article class="col-sm article-border">
                     <h3 style="color:#555;">Physics</h3>
                     <figure>
                         <a href="physics.php" class="logothumbnail">
-                            <img class="img-popup" src="img/physics.jpg" alt="Physics" title="View larger image..." >
+                            <img class="img-popup" src="img/physics.jpg" alt="Physics" title="View larger image...">
                         </a>
                     </figure>
-                    <p style="color: #666; line-height: 1.6;">     
-                    Embark on a thrilling journey into the boundless realms of physics! Physics is the fundamental science that unravels the mysteries of the universe, 
-                    from the smallest particles to the vastness of space and time. By studying physics, you delve into the laws and principles that govern motion, energy, 
-                    and matter, allowing you to comprehend the inner workings of the cosmos. Explore the wonders of quantum mechanics, delve into the mysteries of relativity, 
-                    and uncover the secrets of the forces that shape our world. Embrace the challenge of exploring the unknown and let physics ignite your curiosity, inspire 
-                    your imagination, and propel you toward a deeper understanding of the universe!
+                    <p style="color: #666; line-height: 1.6;">
+                        Embark on a thrilling journey into the boundless realms of physics! Physics is the fundamental science that unravels the mysteries of the universe,
+                        from the smallest particles to the vastness of space and time. By studying physics, you delve into the laws and principles that govern motion, energy,
+                        and matter, allowing you to comprehend the inner workings of the cosmos. Explore the wonders of quantum mechanics, delve into the mysteries of relativity,
+                        and uncover the secrets of the forces that shape our world. Embrace the challenge of exploring the unknown and let physics ignite your curiosity, inspire
+                        your imagination, and propel you toward a deeper understanding of the universe!
                     </p>
                 </article>
             </div>
         </section>
     </main>
     <?php
-        include "inc/footer.inc.php";
+    include "inc/footer.inc.php";
     ?>
 </body>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b104ce9a29e305a153abafae6b50430ac12af101',
+        'reference' => '1041cc1fa7c95969d454035b299546c536792300',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b104ce9a29e305a153abafae6b50430ac12af101',
+            'reference' => '1041cc1fa7c95969d454035b299546c536792300',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
