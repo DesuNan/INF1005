2b705d799b4005fe33cfde927a4173ffd82994e5
darrenh1 <2302021@sit.singaporetech.edu.sg>
2024-04-02 18:57:34 +0800

    changed all forum files to have XSS countermeasures and included some sanitization

--- comment.php
+++ comment.php
@@ -1,16 +1,20 @@
 <div class="comment">
-  <h4><?php echo $data['name']; ?></h4>
-  <p><?php echo $data['date']; ?></p>
-  <p><?php echo $data['comment']; ?></p>
+  <h4><?php echo htmlspecialchars($data['name']); ?></h4>
+  <p><?php echo htmlspecialchars($data['date']); ?></p>
+  <p><?php echo htmlspecialchars($data['comment']); ?></p>
   <?php $reply_id = $data['id']; ?>
-  <button class="reply" onclick = "reply(<?php echo $reply_id; ?>, '<?php echo $data['name']; ?>');">Reply</button>
+  <button class="reply" onclick="reply(<?php echo htmlspecialchars($reply_id); ?>, '<?php echo htmlspecialchars(addslashes($data['name'])); ?>');">Reply</button>
   <?php
   unset($datas);
-  $datas = mysqli_query($conn, "SELECT * FROM tb_data WHERE reply_id = $reply_id");
-  if(mysqli_num_rows($datas) > 0) {
-    foreach($datas as $data){
+  $query = $conn->prepare("SELECT * FROM tb_data WHERE reply_id = ?");
+  $query->bind_param("i", $reply_id);
+  $query->execute();
+  $result = $query->get_result();
+  if($result->num_rows > 0) {
+    while ($data = $result->fetch_assoc()) {
       require 'reply.php';
     }
   }
+  $query->close();
   ?>
 </div>
--- forum.php
+++ forum.php
@@ -1,5 +1,18 @@
+<!DOCTYPE html>
+<html lang=en>
+
 <?php
 $config = parse_ini_file('/var/www/private/db-config-zebra.ini');
+$successFlag = true;
+$name = $comment = $date = $reply_id = "";
+
+function sanitize_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
 if(!$config){
     die("Failed to read database config file.");
 } else {
@@ -9,12 +22,15 @@ if(!$config){
     }
 }
 if(isset($_POST["submit"])){
-    $name = $_POST["name"];
-    $comment = $_POST["comment"];
+    $name = sanitize_input($_POST["name"]);
+    $comment = sanitize_input($_POST["comment"]);
     $date = date('F d Y, h:i:s A');
-    $reply_id = $_POST["reply_id"];
+    $reply_id = sanitize_input($_POST["reply_id"]);
   
-    $query = "INSERT INTO tb_data VALUES('', '$name', '$comment', '$date', '$reply_id')";
+    // Prepare statements before inserting for security
+    $query = $conn->prepare("INSERT INTO tb_data (name, comment, date, reply_id) VALUES (?, ?, ?, ?)");
+    $query->bind_param("sssi", $name, $comment, $date, $reply_id);
+    $query->execute();
     mysqli_query($conn, $query);
 }
 
@@ -72,6 +88,9 @@ if(isset($_POST["submit"])){
     }
   </style>
   <body>
+    <?php 
+    include "INC/nav.inc.php"; 
+    ?>
     <div class="container">
       <?php
       $datas = mysqli_query($conn, "SELECT * FROM tb_data WHERE reply_id = 0"); // only select comment and not select reply
@@ -80,10 +99,10 @@ if(isset($_POST["submit"])){
       }
       ?>
       <form action = "" method = "post">
-        <h3 id = "title">Leave a Comment</h3>
+        <h3 id = "title">Got a question for us? Leave a comment here!</h3>
         <input type="hidden" name="reply_id" id="reply_id">
-        <input type="text" name="name" placeholder="Your name">
-        <textarea name="comment" placeholder="Your comment"></textarea>
+        <input type="text" name="name" placeholder="Your name" required>
+        <textarea name="comment" placeholder="Your comment" required></textarea>
         <button class = "submit" type="submit" name="submit">Submit</button>
       </form>
     </div>
