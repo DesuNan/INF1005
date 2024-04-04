db02ca105d0d161107bd59fdbcc09fb278b2feb1
TzeYaw <144458077+TzeYaw@users.noreply.github.com>
2024-04-04 16:18:32 +0800

    Delete INC directory

--- INC/header.inc.php
+++
@@ -1,4 +0,0 @@
-<header class="jumbotron text-center text-light bg-dark rounded-0">
-    <h1 class="display-4">Welcome to BasicStudys!</h1>
-    <h2>Academy to many different courses</h2>
-</header>
