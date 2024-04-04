5696b602453b82a1414396ea48db263dc600dc58
TzeYaw <hewtzeyaw@gmail.com>
2024-04-04 16:20:35 +0800

    Changed directory of css,img & inc folders and files

---
+++ inc/footer.inc.php
@@ -0,0 +1,14 @@
+<footer id="footer">
+    <div class="container-fluid">
+        <!-- social media icons -->
+        <div class="social-icons mt-4">
+            <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin"></i></a>
+        </div>
+        <div class="container">
+            <p>Copyright &copy; 2024 BasicStudys Pte. Ltd.</p>
+        </div>
+    </div>
+</footer>
