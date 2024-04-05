cce08e8d0a54f07d5112417f53feeca9c26fd1d5
darrenh1 <2302021@sit.singaporetech.edu.sg>
2024-04-05 15:50:30 +0800

    minor additions

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -4,6 +4,8 @@
 <link rel="stylesheet" href="css/main.css">
 <!--Subjects Custom CSS-->
 <link rel="stylesheet" href="css/subject.css">
+<!--Library CSS-->
+<link rel="stylesheet" href="css/library.css">
 <!--Bootsrap JS-->
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
 </script>
