5ee69cf74eb53050c8d8e625e14536243af6a0ff
darrenh1 <2302021@sit.singaporetech.edu.sg>
2024-04-05 21:45:42 +0800

    library changes

--- library/library.php
+++ library/library.php
@@ -36,45 +36,47 @@ if ($conn->connect_error) {
             <a class="btn btn-primary" href="create_entry.php" role="button">New Resource</a>
         <?php endif; ?>
         <br>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Category</th>
-                    <th>Link</th>
-                    <th>Last Updated</th>
-                    <?php if ($_SESSION["accType"] == "instructor") : ?>
-                        <th>Actions</th>
-                    <?php endif; ?>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-
-                $sql = "SELECT * FROM Library";
-                $result = $conn->query($sql);
-
-                while ($row = $result->fetch_assoc()) {
-                    echo "
+        <div class="">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{$row['name']}</td>
-                        <td>{$row['category']}</td>
-                        <td><a href='{$row['link']}' onclick='return confirm(\"Are you sure you want to navigate to this link?\");'>{$row['link']}</a></td>
-                        <td>{$row['last_updated']}</td>";
-                    if ($_SESSION["accType"] == "instructor") {
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Link</th>
+                        <th>Last Updated</th>
+                        <?php if ($_SESSION["accType"] == "instructor") : ?>
+                            <th>Actions</th>
+                        <?php endif; ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+
+                    $sql = "SELECT * FROM Library";
+                    $result = $conn->query($sql);
+
+                    while ($row = $result->fetch_assoc()) {
                         echo "
-                            <td>
-                                <a class='btn btn-secondary btn-sm' href='edit_entry.php?id={$row['id']}'>Edit</a> 
-                                <a class='btn btn-danger btn-sm' href='delete_entry.php?id={$row['id']}' onclick='return confirm(\"Are you sure you want to delete this?\");'>Delete</a> 
-                            </td>
-                        </tr>";
-                    } else {
-                        echo "</tr";
+                        <tr>
+                            <td>{$row['name']}</td>
+                            <td>{$row['category']}</td>
+                            <td><a href='{$row['link']}' onclick='return confirm(\"Are you sure you want to navigate to this link?\");'>{$row['link']}</a></td>
+                            <td>{$row['last_updated']}</td>";
+                        if ($_SESSION["accType"] == "instructor") {
+                            echo "
+                                <td>
+                                    <a class='btn btn-secondary btn-sm' href='edit_entry.php?id={$row['id']}'>Edit</a> 
+                                    <a class='btn btn-danger btn-sm' href='delete_entry.php?id={$row['id']}' onclick='return confirm(\"Are you sure you want to delete this?\");'>Delete</a> 
+                                </td>
+                            </tr>";
+                        } else {
+                            echo "</tr";
+                        }
                     }
-                }
-                ?>
-            </tbody>
-        </table>
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
 
