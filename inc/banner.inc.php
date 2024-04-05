b6c9903ad9d913923b4ebcaaf5e893b2f3c843c7
darrenh1 <2302021@sit.singaporetech.edu.sg>
2024-04-05 17:03:58 +0800

    a

--- inc/banner.inc.php
+++ inc/banner.inc.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
                 <h2 class="hero_title">Welcome to our website</h2>
-                <p class="hero_desc">We specilized in teaching for your future</p>
+                <p class="hero_desc">We specialize in teaching for your future</p>
             </div>
         </div>
     </div>
